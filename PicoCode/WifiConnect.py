7e9d30672d5a49e196b414a8f957e3e52679a81c
Andrew Gurges <gurgea1@mcmaster.ca>
2024-01-18 17:56:02 -0500

    Adds basic wifi setup and AP mode

--- PicoCode/WifiConnect.py
+++ PicoCode/WifiConnect.py
@@ -12,6 +12,25 @@ def led_blink():
     pico_led.off()
     sleep(0.1)
 
+def SetupWifiInfo():
+    f = open('WifiInfo.txt', 'w')
+    # Used for testing, this should be taken from the app
+    print("Enter SSID and Password:")
+    ssid = input("SSID: ")
+    password = input("Password: ")
+    # Store the wifi login information locally to be used later
+    f.write(f"{ssid}, {password}")
+    f.close()
+    return ssid, password
+
+
+def APMode():
+    # Turns on AP mode in powersaving for the phone to access
+    ap = network.WLAN(network.AP_IF)
+    ap.config(ssid = "PlugPico", password = "Capstone", pm = network.WLAN.PM_POWERSAVE)
+    ap.active(True)
+    return
+
 def connect(ssid, password):
     wlan = network.WLAN(network.STA_IF)
     timecount = 0
@@ -26,6 +45,10 @@ def connect(ssid, password):
               sleep(1)
               attemptcount += 1
         timecount += 1
+    # Device was unable to connect to the given wifi signal
+    if(timecount == TIMEOUT_COUNT):
+        print("Unable to connect.")
+        return -1
     # Double blink to show device is connected
     led_blink()
     led_blink()

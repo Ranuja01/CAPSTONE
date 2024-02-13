7677e305826887b6397840eeca3baab8f0071446
Andrew Gurges <gurgea1@mcmaster.ca>
2024-02-12 22:06:19 -0500

    Refactored HTTP Requests, added current tracking

--- PicoCode/TestHTTPReq.py
+++ PicoCode/TestHTTPReq.py
@@ -1,8 +1,10 @@
 # Andrew Gurges Capstone Code
 import WifiConnect as wc
+import HTTPReq as httpr
 import urequests as requests
 import network
 import machine
+from time import sleep
 
 # ssid = 'ssid'
 # password = 'password'
@@ -10,58 +12,96 @@ import machine
 hostname = 'PicoPlug'
 phoneIP = '192.168.1.103'
 
-def webpage(voltage, current, state):
-    # Basic HTML
-    html = f"""
-    <!DOCTYPE html>
-    <html>
-    <form action="./poweron">
-    <input type="submit" value="Power on" />
-    </form>
-    <form action="./poweroff">
-    <input type="submit" value="Power off" />
-    </form>
-    <p>LED is {state}</p>
-    <p>Current is {current}</p>
-    <p>Voltage is {voltage}</p>
-    </body>
-    </html>
-    """
-    return str(html)
 
 def setHostname(ip, hostname):
+    ''' Sets the hostname of the plug '''
     res = requests.get(url=f'http://{ip}/cm?cmnd=Hostname%20{hostname}')
     return res.text
 
 def getVoltCurr(ip):
-    # Sends a Tasmota command (Status 10) Tasmota which returns the voltage and current readings in a dict
-    res = requests.get(url=f'http://{ip}/cm?cmnd=Status%2010')
-    # Converts string output to dict
-    resdict = eval(res.text)
-    # Extracts the voltage and current readings
-    voltage = resdict['StatusSNS']['ENERGY']['Voltage']
-    current = resdict['StatusSNS']['ENERGY']['Current']
+    ''' Reads the voltage and current information from the plug '''
+    try:
+        # Sends a Tasmota command (Status 10) Tasmota which returns the voltage and current readings in a dict
+        res = requests.get(url=f'http://{ip}/cm?cmnd=Status%2010')
+        # Converts string output to dict
+        resdict = eval(res.text)
+        # Extracts the voltage and current readings
+        voltage = resdict['StatusSNS']['ENERGY']['Voltage']
+        current = resdict['StatusSNS']['ENERGY']['Current']
+    except OSError:
+        print("Unable to read voltage or current.")
+        voltage = -1
+        current = -1
     return voltage, current
 
 def setPower(command):
-    # Toggle the smart plug power
-    response = requests.get(url=f'http://{hostname}/cm?cmnd=Power%20{command}')
+    ''' Toggle the smart plug power '''
+    try:
+        response = requests.get(url=f'http://{hostname}/cm?cmnd=Power%20{command}')
+    except:
+        print("Could not toggle power")
+        return ""
     return response.text
 
-try:
+def readCurrents():
+    # Array of current values and kepeing track of the end 
+    currVals = 10*[0.0]
+    end = 0
+    initSize = 10
+    avg = 0
+    currsum = 0
+    
+    # Get the first few values to fill the array
+    while end < initSize:
+        voltage, current = getVoltCurr(hostname)
+        print("Current = ", current)
+        # Only use nonzero current values to fill the array
+        if(current != 0):
+            currVals[end] = current
+            end += 1
+        sleep(0.5)
+        
+    print(currVals)
+    
+    for ele in currVals:
+            currsum += ele
+    avg = currsum / len(currVals)
+    end = 0
+    while(True):
+        voltage, current = getVoltCurr(hostname)
+        print(f"Current = {current}")
+        print(f"Average = {avg}")
+        # Remove old value and add new value to sum, update array and average
+        currsum -= currVals[end]
+        currsum += current
+        currVals[end] = current
+        avg = currsum / len(currVals)
+        end = (end + 1) % initSize # 0-initSize then loops back to 0
+        sleep(0.5)
+        
+    return
 
+try:
     ip = -1
     while(ip == -1):
         # Get the ssid and password and store it locally
-        ssid, password = wc.setupWifiInfo()
+        inp = input("Use existing info? (Y/N)")
+        ssid, password = wc.setupWifiInfo(inp=='Y')
         # Connect to wifi given the ssid and password
-        ip = wc.connect(ssid, password)
+        ip = wc.connectWifi(ssid, password)
+        
+    # Open a socket for http requests
+    connection = httpr.open_socket(ip)
+    state = 'OFF'
+    
     while(True):
-        # Open a socket for http requests
-        connection = wc.open_socket(ip)
+        readCurrents()
+    
+    while(True):
         # Connect to the Phone's IP
         # res = requests.get(url=f'http://{phoneIP}:8080/Send')
-        client, request = wc.parseRequest(connection)
+        client, request = httpr.parseRequest(connection)
+        print(request)
         
         if request == '/poweron?':
             setPower('ON')
@@ -78,20 +118,14 @@ try:
         
         voltage, current = getVoltCurr(hostname)
         
-        html = webpage(voltage, current, state)
+        html = httpr.webpage(voltage, current, state)
         client.send(html)
         client.close()
         
         
         print(f"Voltage = {voltage}")
         print(f"Current = {current}")
-        '''
-        tog = input("Toggle? Y/N (E to exit): ")
-        if (tog == 'Y'):
-            print(togglePower(hostname))
-        if (tog == 'E'):
-            break
-        '''
-            
+
+
 except KeyboardInterrupt:
     machine.reset()
--- PicoCode/WifiConnect.py
+++ PicoCode/WifiConnect.py
@@ -4,35 +4,53 @@ from time import sleep
 from picozero import pico_led
 import machine
 
-CONN_RESET_COUNT = 10
-TIMEOUT_COUNT = 4
+CONN_RESET_COUNT = 5
+TIMEOUT_COUNT = 2
 
 def led_blink():
+    '''Blinks the LED'''
     pico_led.on()
     sleep(0.1)
     pico_led.off()
     sleep(0.1)
 
-def SetupWifiInfo():
-    f = open('WifiInfo.txt', 'w')
-    # Used for testing, this should be taken from the app
-    print("Enter SSID and Password:")
-    ssid = input("SSID: ")
-    password = input("Password: ")
-    # Store the wifi login information locally to be used later
-    f.write(f"{ssid}, {password}")
-    f.close()
+def setupWifiInfo(useExisting):
+    ''' Determines whether wifi information is input or read from a file '''
+    if(useExisting):
+        f = open('WifiInfo.txt', 'r')
+        info = f.readline().split(',')
+        ssid = info[0]
+        password = info[1]
+        f.close()
+    else:
+        f = open('WifiInfo.txt', 'w')
+        # Used for testing, this should be taken from the app
+        print("Enter SSID and Password:")
+        ssid = input("SSID: ")
+        password = input("Password: ")
+        # Store the wifi login information locally to be used later
+        f.write(f"{ssid},{password}")
+        f.close()
+    print(f"Using SSID: {ssid} and Password: {password}")
     return ssid, password
 
 
-def APMode():
-    # Turns on AP mode in powersaving for the phone to access
+def APModeSetup(ip, ap):
+    '''Turns on AP mode in powersaving for the phone to access '''
     ap = network.WLAN(network.AP_IF)
     ap.config(ssid = "PlugPico", password = "Capstone", pm = network.WLAN.PM_POWERSAVE)
     ap.active(True)
+    ip = ap.ifconfig()[0]
+    return ip, ap
+
+def APModeDisconnect(ap):
+    ''' Turns off AP Mode '''
+    ap.active(False)
     return
 
-def connect(ssid, password):
+
+def connectWifi(ssid, password):
+    ''' Attempt to connect to the wifi network given the SSID and Password '''
     wlan = network.WLAN(network.STA_IF)
     timecount = 0
     # Retry connection until timeout reached
@@ -54,24 +72,7 @@ def connect(ssid, password):
     led_blink()
     led_blink()
     ip = wlan.ifconfig()[0]
+    
     print(f'Connected on {ip}')
     return ip
 
-def open_socket(ip):
-    # Open a socket
-    address = (ip, 80) # 80 <- Port Number
-    connection = socket.socket()
-    connection.bind(address)
-    connection.listen(1)
-    return connection
-
-def parseRequest(connection):
-    client = connection.accept()[0]
-    request = client.recv(1024)
-    request = str(request)
-    print(request.split()[1])
-    try:
-        request = request.split()[1] # Request has format http://{IP}/poweron?
-    except IndexError:
-        pass
-    return client, request
